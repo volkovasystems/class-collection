0322c7ee83d7a49c8904b3e07d33c15450c14388
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-02-11 09:22:27 +0800

    Added class Collection.

---
+++ class-collection.js
@@ -0,0 +1,15 @@
+/*:
+*/
+Collection = function Collection( collection ){
+	/*:
+		@meta-configuration:
+		@end-meta-configuration
+	*/
+};
+
+Collection.prototype.getCurrentCount = function getCurrentCount( ){
+
+};
+
+
+
